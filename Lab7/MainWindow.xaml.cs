c2f122b7b78e48c702f5b40d17b31497b7feaec0
akshipulya <akshipulya@gmail.com>
2022-05-24 16:52:09 +0300

    24/05/2022

---
+++ Lab7/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person;
+        ObservableCollection<Person> collection = new ObservableCollection<Person>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            person = new Person();
+            stackpanelPerson.DataContext = person;
+            listBox.DataContext = collection;
+            FillData();
+        }
+
+        private void buttonView_Click(object sender, RoutedEventArgs e)
+        {
+            // MessageBox.Show(person.ToString());
+            FillData();
+        }
+        private void FillData()
+        {
+            collection.Clear();
+            foreach (var p in Person.getAllPersons())
+            {
+                collection.Add(p);
+            }
+        }
+
+        private void buttonInsert_Click(object sender, RoutedEventArgs e)
+        {
+            person.Insert();
+            FillData();
+        }
+
+        private void buttonFind_Click(object sender, RoutedEventArgs e)
+        {
+            person = Person.Find(textBoxName.Text);
+            if (person == null)
+            {
+                MessageBox.Show("Нет такой записи!");
+                person = new Person();
+            }
+            else
+                MessageBox.Show(person.ToString());
+        }
+
+        private void buttonChange_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Нет выбранной записи!");
+                return;
+            }
+            person.Id = ((Person)listBox.SelectedItem).Id;
+
+            if (textBoxName.Text.Length > 0)
+                person.Name = textBoxName.Text;
+            else
+                person.Name = ((Person)listBox.SelectedItem).Name;
+            decimal d = Convert.ToDecimal(textBoxSum.Text);
+            if (d != 0)
+                person.Sum = d;
+            else
+                person.Sum = ((Person)listBox.SelectedItem).Sum;
+            person.Update();
+            FillData();
+        }
+
+        private void buttonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Нет выбранной записи!");
+                return;
+            }
+            var id = ((Person)listBox.SelectedItem).Id;
+            Person.Delete(id);
+            FillData();
+        }
+    }
+}
---
+++ Lab7/Person.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    internal class Person
+    {
+        static SqlConnection connection;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Sum { get; set; }
+
+        static Person()
+        {
+            var connstring = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            connection = new SqlConnection(connstring);
+        }
+        private static void openConnection()
+        {
+            // из-за метода getAllPersons() подключение может оставаться открытым
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+            connection.Open();
+        }
+
+        public static IEnumerable<Person> getAllPersons()
+        {
+            openConnection();
+            var SQLstr = "SELECT * FROM Person";
+            SqlCommand getAllCommand = new SqlCommand(SQLstr, connection);
+            var reader = getAllCommand.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    var id = reader.GetInt32(0);
+                    var name = reader.GetString(1);
+                    var sum = reader.GetDecimal(2);
+                    var person = new Person() { Id = id, Name = name, Sum = sum };
+                    yield return person;
+                }
+            }
+            connection.Close();
+        }
+        public void Insert()
+        {
+            openConnection();
+            var SQLstr = "INSERT INTO Person (Name, Sum) VALUES (@name, @sum)";
+            SqlCommand insertCommand = new SqlCommand(SQLstr, connection);
+            insertCommand.Parameters.AddRange(new SqlParameter[]
+            {
+                new SqlParameter("name", Name),
+                new SqlParameter("sum", Sum)
+            });
+            insertCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public static void Delete(int id)
+        {
+            openConnection();
+            var commandString = "DELETE FROM Person WHERE(Id=@id)";
+            SqlCommand deleteCommand = new SqlCommand(commandString, connection);
+            deleteCommand.Parameters.AddWithValue("id", id);
+            deleteCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public static Person Find(string name)
+        {
+            foreach (var person in getAllPersons())
+            {
+                if (person.Name == name)
+                    return person;
+            }
+            return null;
+        }
+
+        public void Update()
+        {
+            openConnection();
+            var commandString = "UPDATE Person SET Name=@name, Sum=@sum WHERE(Id=@id)";
+            SqlCommand updateCommand = new SqlCommand(commandString, connection);
+            updateCommand.Parameters.AddRange(new SqlParameter[] {
+                             new SqlParameter("name", Name),
+                             new SqlParameter("sum", Sum),
+                             new SqlParameter("id", Id),});
+            updateCommand.ExecuteNonQuery();
+            connection.Close();
+        }
+
+
+        public override string ToString()
+        {
+            return $"№ {Id} Имя: {Name}, Сумма: {Sum:0.000}";
+        }
+    }
+
+}
